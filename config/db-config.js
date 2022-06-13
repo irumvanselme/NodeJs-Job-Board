b5210d0314d84a413a3265c41a8e177c58140dbd
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2022-06-13 09:33:47 +0200

    feat: done with the dockerizing the application

--- config/db-config.js
+++ config/db-config.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://data-store:27017/nat-examination").then((res) => {
-    console.log("Connected to the database");
-});
+mongoose
+    .connect("mongodb://data-store:27017/nat-examination")
+    .then((res) => {
+        console.log("Connected to the database");
+    })
+    .catch((e) => {
+        console.log("Failed to connect to the database");
+        process.exit(0);
+    });
 
 require("../models/user-model");
