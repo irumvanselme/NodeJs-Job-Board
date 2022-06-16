549019a84322eff5982df0ae46e7067c53577627
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2022-06-16 14:43:55 +0200

    feat: implement get all users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,8 +1,12 @@
 
 // make this for only admin ...
 
+const { User } = require("../models/user-model")
+
 async function all(req, res) {
-    return res.send("All users here")
+    let users = await User.find();
+
+    return res.send(users)
 }
 
 module.exports = {
